2130445f09665dba3b4af0263a6bb5ce562c6a0a
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2023-05-06 19:26:22 -0400

    updated CreateInterary.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CssBaseline, Box } from "@mui/material";
-import LandingPage from "./LandingPage";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import CreateItinerary from "./CreateItinerary";
+// import LandingPage from "./components/HomePage";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import CreateItinerary from "./pages/CreateItinerary";
 
 const App = () => (
   <>
@@ -12,7 +12,6 @@ const App = () => (
     <Box minHeight="calc(100vh - 150px)">
       <Navbar />
       <CreateItinerary />
-      <LandingPage />
     </Box>
     <Footer />
   </>
