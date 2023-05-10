3528e0d128d8dce4773de9b40cfb3cb50d4a7418
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-05-10 18:42:57 -0400

    user routes, model and controller

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/seat_geek';
-
-connect(connectionStr, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/seat_geek', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+    useCreateIndex: true,
+  });
 
-module.exports = connection;
+module.exports = mongoose.connection;
