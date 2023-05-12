239a10e893caa99202474ae297e68d1ec7f31a1e
jjsmith32199 <113246711+jjsmith32199@users.noreply.github.com>
2023-05-11 21:47:16 -0400

    removed models that were not needed"

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
 
   
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
