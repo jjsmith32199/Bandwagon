48cb52fee30a971aaf16c8bba0d5246e362c5ead
jjsmith32199 <113246711+jjsmith32199@users.noreply.github.com>
2023-05-11 21:39:26 -0400

    fixed mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/seat_geek', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/seat_geek', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
