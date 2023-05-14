5155d392c265d1a04835a78ac7788027ad851652
itsHaso <117330115+itsHaso@users.noreply.github.com>
2023-05-13 22:00:58 -0400

    added importData.js and testServer changes
    added importData.js to import user data to mongodb added the server.js code to testserver testserver runs properly in playground but server.js does not shows a white page on graphql link click

--- server/schemas/resolvers/artist/artistResolvers.js
+++ server/schemas/resolvers/artist/artistResolvers.js
@@ -10,15 +10,13 @@ const resolvers = {
                 populate: 'venue'
             });
 
-            // Map the MongoDB documents to match the GraphQL schema
             return artists.map(artist => ({
                 id: artist._id.toString(),
                 name: artist.artist_name,
                 genre: artist.genre,
-                venue: artist.venue, // You might need to adjust this if `venue` is also an object with a different structure in MongoDB
+                venue: artist.venue, 
             }));
         },
-        // ...
     }
 };
 
